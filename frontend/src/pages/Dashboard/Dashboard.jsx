d9f8d40afe0cf42723cd56ee7273ab834649a6fa
chyvacheck <work.dmytro02@gmail.com>
2023-12-23 17:14:15 +0100

    fix: добавить key в списки

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -96,9 +96,19 @@ function Dashboard({ addNotification }) {
           <h3 className={`title-second ${s.title}`}>Wallets</h3>
           <table className={s.table}>
             <thead>
-              <tr>
-                <th className={`subhead ${s.table__title}`}>Name</th>
-                <th className={`subhead ${s.table__title}`}>Value</th>
+              <tr key={`tb1_thead_tr1`}>
+                <th
+                  key={`tb1_thead_th1`}
+                  className={`subhead ${s.table__title}`}
+                >
+                  Name
+                </th>
+                <th
+                  key={`tb1_thead_th2`}
+                  className={`subhead ${s.table__title}`}
+                >
+                  Value
+                </th>
               </tr>
             </thead>
             <tbody>
@@ -107,14 +117,21 @@ function Dashboard({ addNotification }) {
                 return (
                   <>
                     {index > 0 && (
-                      <tr>
-                        <td className={s.line} />
-                        <td className={s.line} />
+                      <tr key={`tb1_tr1_in_${index}`}>
+                        <td
+                          key={`tb1_tr1_td1_in_${index}`}
+                          className={s.line}
+                        />
+                        <td
+                          key={`tb1_tr1_td2_in_${index}`}
+                          className={s.line}
+                        />
                       </tr>
                     )}
 
-                    <tr>
+                    <tr key={`tb1_tr2_in_${index}`}>
                       <td
+                        key={`tb1_tr2_td1_in_${index}`}
                         onClick={() => {
                           copy(wallet._id);
                         }}
@@ -123,7 +140,7 @@ function Dashboard({ addNotification }) {
                         id:{' '}
                         <span className={`copy ${s.value}`}>{wallet._id}</span>
                       </td>
-                      <td />
+                      <td key={`tb1_tr2_td2_in_${index}`} />
                     </tr>
 
                     {Object.keys(wallet.currency).map((currency, _index) => {
@@ -133,16 +150,16 @@ function Dashboard({ addNotification }) {
 
                       return (
                         _isShow && (
-                          <tr key={`tr_${wallet._id}_${index}_${_index}`}>
+                          <tr key={`tb1_tr3_in_${index}_in2_${_index}`}>
                             <td
+                              key={`tb1_tr3_td1_in_${index}`}
                               className={`caption ${s.table__price}`}
-                              key={`td_1_${wallet._id}_${index}`}
                             >
                               {currency}
                             </td>
                             <td
+                              key={`tb1_tr3_td2_in_${index}`}
                               className={`caption ${s.table__price}`}
-                              key={`td_2_${wallet._id}_${index}`}
                             >
                               {wallet.currency[currency]}
                             </td>
@@ -159,92 +176,121 @@ function Dashboard({ addNotification }) {
         {/* // ? транзакции */}
         <div>
           <h3 className={`title-second ${s.title}`}>Transactions</h3>
-          <table className={s.table}>
-            <thead>
-              <tr>
-                {/* <th className={`subhead ${s.table__title}`}>Id</th>
-              <th className={`subhead ${s.table__title}`}>Received</th>
-              <th className={`subhead ${s.table__title}`}>Given</th>
-              <th className={`subhead ${s.table__title}`}>Time</th> */}
+          {userData.transactions.length > 0 ? (
+            <table className={s.table}>
+              <thead>
+                <tr key={`tb2_thead_tr1`}>
+                  <th className={`subhead ${s.table__title}`}>Name</th>
+                  <th className={`subhead ${s.table__title}`}>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* Мапим данные для каждой криптовалюты */}
+                {userData.transactions.map((tr, index) => {
+                  const date = new Date(tr.data);
 
-                <th className={`subhead ${s.table__title}`}>Name</th>
-                <th className={`subhead ${s.table__title}`}>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* Мапим данные для каждой криптовалюты */}
-              {userData.transactions.map((tr, index) => {
-                const date = new Date(tr.data);
+                  const time = `${date.getFullYear()}/${
+                    date.getMonth() < 10
+                      ? `0${date.getMonth()}`
+                      : date.getMonth()
+                  }/${
+                    date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
+                  } ${
+                    date.getHours() < 10
+                      ? `0${date.getHours()}`
+                      : date.getHours()
+                  }:${
+                    date.getMinutes() < 10
+                      ? `0${date.getMinutes()}`
+                      : date.getMinutes()
+                  }:${
+                    date.getSeconds() < 10
+                      ? `0${date.getSeconds()}`
+                      : date.getSeconds()
+                  }`;
 
-                const time = `${date.getFullYear()}/${
-                  date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth()
-                }/${
-                  date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
-                } ${
-                  date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()
-                }:${
-                  date.getMinutes() < 10
-                    ? `0${date.getMinutes()}`
-                    : date.getMinutes()
-                }:${
-                  date.getSeconds() < 10
-                    ? `0${date.getSeconds()}`
-                    : date.getSeconds()
-                }`;
-
-                return (
-                  <>
-                    {index > 0 && (
-                      <tr>
-                        <td className={s.line} />
-                        <td className={s.line} />
+                  return (
+                    <>
+                      {index > 0 && (
+                        <tr key={`tb2_tr1_in_${index}`}>
+                          <td
+                            key={`tb2_tr1_td1_in_${index}`}
+                            className={s.line}
+                          />
+                          <td
+                            key={`tb2_tr1_td2_in_${index}`}
+                            className={s.line}
+                          />
+                        </tr>
+                      )}
+                      {/* // ? id */}
+                      <tr key={`tb2_tr2_in_${index}_id_${tr._id}`}>
+                        <td
+                          key={`tb2_tr2_td1_in_${index}`}
+                          onClick={() => {
+                            copy(tr._id);
+                          }}
+                          className={`caption ${s.table__price}`}
+                        >
+                          id:{' '}
+                          <span className={`copy ${s.value}`}>{tr._id}</span>
+                        </td>
+                        <td key={`tb2_tr2_td2_in_${index}`} />
                       </tr>
-                    )}
-                    {/* // ? id */}
-                    <tr key={`tr_1_${tr._id}_${index}`}>
-                      <td
-                        onClick={() => {
-                          copy(tr._id);
-                        }}
-                        className={`caption ${s.table__price}`}
-                      >
-                        id: <span className={`copy ${s.value}`}>{tr._id}</span>
-                      </td>
-                      <td />
-                    </tr>
-                    {/* // ? received */}
-                    <tr key={`tr_2_${tr._id}_${index}`}>
-                      <td className={`caption ${s.table__price}`}>received</td>
-                      <td
-                        className={`caption copy ${s.value} ${s.table__price}`}
-                      >
-                        {tr.received.value} {tr.received.currency}
-                      </td>
-                    </tr>
-                    {/* // ? given */}
-                    <tr key={`tr_3_${tr._id}_${index}`}>
-                      <td className={`caption ${s.table__price}`}>given</td>
-                      <td
-                        className={`caption copy ${s.value} ${s.table__price}`}
-                      >
-                        {tr.given.value} {tr.given.currency}
-                      </td>
-                    </tr>
-                    {/* // ? date */}
-                    <tr key={`tr_4_${tr._id}_${index}`}>
-                      <td className={`caption ${s.table__price}`}>date</td>
-                      <td
-                        onClick={() => copy(time)}
-                        className={`caption copy ${s.value} ${s.table__price}`}
-                      >
-                        {time}
-                      </td>
-                    </tr>
-                  </>
-                );
-              })}
-            </tbody>
-          </table>
+                      {/* // ? received */}
+                      <tr key={`tb2_tr3_in_${index}_id_${tr._id}`}>
+                        <td
+                          key={`tb2_tr3_td1_in_${index}`}
+                          className={`caption ${s.table__price}`}
+                        >
+                          received
+                        </td>
+                        <td
+                          key={`tb2_tr3_td2_in_${index}`}
+                          className={`caption copy ${s.value} ${s.table__price}`}
+                        >
+                          {tr.received.value} {tr.received.currency}
+                        </td>
+                      </tr>
+                      {/* // ? given */}
+                      <tr key={`tb2_tr4_in_${index}_id_${tr._id}`}>
+                        <td
+                          key={`tb2_tr4_td1_in_${index}`}
+                          className={`caption ${s.table__price}`}
+                        >
+                          given
+                        </td>
+                        <td
+                          key={`tb2_tr4_td2_in_${index}`}
+                          className={`caption copy ${s.value} ${s.table__price}`}
+                        >
+                          {tr.given.value} {tr.given.currency}
+                        </td>
+                      </tr>
+                      {/* // ? date */}
+                      <tr key={`tb2_tr5_in_${index}_id_${tr._id}`}>
+                        <td
+                          key={`tb2_tr5_td1_in_${index}`}
+                          className={`caption ${s.table__price}`}
+                        >
+                          date
+                        </td>
+                        <td
+                          key={`tb2_tr5_td2_in_${index}`}
+                          onClick={() => copy(time)}
+                          className={`caption copy ${s.value} ${s.table__price}`}
+                        >
+                          {time}
+                        </td>
+                      </tr>
+                    </>
+                  );
+                })}
+              </tbody>
+            </table>
+          ) : (
+            <p className={`${s.info}`}>User don't have any transactions yet</p>
+          )}
         </div>
       </article>
 
@@ -262,10 +308,10 @@ function Dashboard({ addNotification }) {
                   <th className={`subhead ${s.table__title}`}>Name</th>
 
                   {/* Добавьте столбцы для остальных валют */}
-                  {Object.keys(allPrices[0].data).map((currency) => (
+                  {Object.keys(allPrices[0].data).map((currency, index) => (
                     <th
                       className={`caption ${s.table__title}`}
-                      key={`${currency}_t1_h_c1`}
+                      key={`tb3_th2_in_${index}_currency_${currency}`}
                     >
                       {currency.toUpperCase()}
                     </th>
@@ -274,29 +320,27 @@ function Dashboard({ addNotification }) {
               </thead>
               <tbody>
                 {/* Мапим данные для каждой криптовалюты */}
-                {allPrices.map((crypto) => {
+                {allPrices.map((crypto, index) => {
                   return (
-                    <tr key={`tr_${crypto.name}`}>
+                    <tr key={`tb3_tr1_in_${index}_name_${crypto.name}`}>
                       <td
                         className={`caption ${s.table__price}`}
-                        key={`${crypto.name}_t1_b_n1`}
+                        key={`tb3_td1_in_${index}_name_${crypto.name}`}
                       >
                         {crypto.name}
                       </td>
 
-                      {Object.keys(crypto.data).map((currency) => (
-                        <>
-                          <td
-                            className={`detail ${s.table__price}`}
-                            key={`${currency}_t1_b_c1`}
-                          >
-                            {Number(
-                              crypto.data[currency].toPrecision(
-                                VALIDATION.PRICE.TO_PRECISION,
-                              ),
-                            )}
-                          </td>
-                        </>
+                      {Object.keys(crypto.data).map((currency, _index) => (
+                        <td
+                          className={`detail ${s.table__price}`}
+                          key={`tb3_td2_in_${index}_in2_${_index}`}
+                        >
+                          {Number(
+                            crypto.data[currency].toPrecision(
+                              VALIDATION.PRICE.TO_PRECISION,
+                            ),
+                          )}
+                        </td>
                       ))}
                     </tr>
                   );
@@ -315,18 +359,22 @@ function Dashboard({ addNotification }) {
           User crypto currency in usd and eur
         </h3>
         {isPricesLoad ? (
-          <div>
+          <>
             {hasUserCurrency && (
               <table className={s.table}>
                 <thead>
                   <tr>
-                    <th className={`subhead ${s.table__title}`}>Name</th>
-                    <th className={`subhead ${s.table__title}`}>User wallet</th>
+                    <th key={`tb4_th1`} className={`subhead ${s.table__title}`}>
+                      Name
+                    </th>
+                    <th key={`tb4_th2`} className={`subhead ${s.table__title}`}>
+                      User wallet
+                    </th>
                     {/* Добавьте столбцы для остальных валют */}
-                    {Object.keys(allPrices[0].data).map((currency) => (
+                    {Object.keys(allPrices[0].data).map((currency, index) => (
                       <th
                         className={`caption ${s.table__title}`}
-                        key={`${currency}_t2_h_c1`}
+                        key={`tb4_th3_in_${index}`}
                       >
                         {currency.toUpperCase()}
                       </th>
@@ -335,7 +383,7 @@ function Dashboard({ addNotification }) {
                 </thead>
                 <tbody>
                   {/* Мапим данные для каждой криптовалюты */}
-                  {allPrices.map((crypto) => {
+                  {allPrices.map((crypto, index) => {
                     const _value = Number(
                       userData.wallets[0].currency[crypto.name].toPrecision(
                         VALIDATION.PRICE.TO_PRECISION,
@@ -344,26 +392,26 @@ function Dashboard({ addNotification }) {
 
                     return (
                       _value > 0 && (
-                        <tr key={`tr_${crypto.name}`}>
+                        <tr key={`tb4_tr1_in_${index}`}>
                           <td
-                            key={`${crypto.name}_t2_b_n1`}
+                            key={`tb4_td1_in_${index}`}
                             className={`caption ${s.table__price}`}
                           >
                             {crypto.name}
                           </td>
                           <td
-                            key={`${crypto.name}_t2_b_n2`}
+                            key={`tb4_td2_in_${index}`}
                             className={`caption ${s.table__price}`}
                           >
                             {_value}
                           </td>
 
                           {/* Добавьте ячейки для остальных валют */}
-                          {Object.keys(crypto.data).map((currency) => {
+                          {Object.keys(crypto.data).map((currency, _index) => {
                             return (
                               <td
                                 className={`detail ${s.table__price}`}
-                                key={`${currency}_t2_b_c1`}
+                                key={`tb4_td3_in_${index}_in2_${_index}`}
                               >
                                 {Number(
                                   crypto.data[currency].toPrecision(
@@ -385,7 +433,7 @@ function Dashboard({ addNotification }) {
                 User don't have any crypto currency yet
               </p>
             )}
-          </div>
+          </>
         ) : (
           'Waiting...'
         )}
